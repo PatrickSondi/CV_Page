5a71cdb54af92ec171af51aa939b6c8b47fd1f84
Patrick <patrick@psonderegger.at>
2018-03-02 19:04:08 +0100

    TypeIt implement

--- js/init.js
+++ js/init.js
@@ -1,29 +1,25 @@
-/*-----------------------------------------------------------------------------------
-/*
-/* Init JS
-/*
------------------------------------------------------------------------------------*/
-
- jQuery(document).ready(function($) {
-
-/*----------------------------------------------------*/
-/* FitText Settings
------------------------------------------------------- */
-
+jQuery(document).ready(function($) {
+    /* FitText Settings */
     setTimeout(function() {
-	   $('h1.responsive-headline').fitText(1, { minFontSize: '40px', maxFontSize: '90px' });
-	 }, 100);
+        $('h1.responsive-headline').fitText(1, { minFontSize: '40px', maxFontSize: '90px' });
+    }, 100);
+        
+    /* TypeIt initialize */
+    new TypeIt('#typewriter', {
+        strings: "I am Patrick Sonderegger, a Vienna based <span>frontend developer</span> with an affinity for <span>backend development</span>. Let's <a class='smoothscroll' href='#about'>start scrolling</a> and learn more <a class='smoothscroll' href='#about'>about me</a>.",
+        speed: 70,
+        autoStart: true,
+        callback: function() {
+            $('.scrolldown').addClass("jello-horizontal");
+        }
+    });
 
-
-/*----------------------------------------------------*/
-/* Smooth Scrolling
------------------------------------------------------- */
-
+    /* Smooth Scrolling */
    $('.smoothscroll').on('click',function (e) {
 	    e.preventDefault();
 
-	    var target = this.hash,
-	    $target = $(target);
+	    var target = this.hash;
+	    var $target = $(target);
 
 	    $('html, body').stop().animate({
 	        'scrollTop': $target.offset().top
@@ -32,151 +28,109 @@
 	    });
 	});
 
-
-/*----------------------------------------------------*/
-/* Highlight the current section in the navigation bar
-------------------------------------------------------*/
-
+    /* Highlight the current section in the navigation bar */
 	var sections = $("section");
 	var navigation_links = $("#nav-wrap a");
 
 	sections.waypoint({
-
-      handler: function(event, direction) {
-
-		   var active_section;
-
-			active_section = $(this);
-			if (direction === "up") active_section = active_section.prev();
+        handler: function(event, direction) {
+            var active_section = $(this);
+            
+            if (direction === "up") {
+                active_section = active_section.prev();
+            } 
 
 			var active_link = $('#nav-wrap a[href="#' + active_section.attr("id") + '"]');
 
-         navigation_links.parent().removeClass("current");
+            navigation_links.parent().removeClass("current");
 			active_link.parent().addClass("current");
-
 		},
 		offset: '35%'
-
 	});
 
+    /* #header-background-image height equal to browser height. */
+    $('header').css({ 
+        'height': $(window).height() 
+    });
 
-/*----------------------------------------------------*/
-/*	Make sure that #header-background-image height is
-/* equal to the browser height.
------------------------------------------------------- */
-
-   $('header').css({ 'height': $(window).height() });
-   $(window).on('resize', function() {
-
+    $(window).on('resize', function() {
         $('header').css({ 'height': $(window).height() });
         $('body').css({ 'width': $(window).width() })
-   });
+    });
 
-
-/*----------------------------------------------------*/
-/*	Fade In/Out Primary Navigation
-------------------------------------------------------*/
-
-   $(window).on('scroll', function() {
-
+    /* Fade In/Out Primary Navigation */
+    $(window).on('scroll', function() {
 		var h = $('header').height();
 		var y = $(window).scrollTop();
-      var nav = $('#nav-wrap');
+        var nav = $('#nav-wrap');
 
-	   if ( (y > h*.20) && (y < h) && ($(window).outerWidth() > 768 ) ) {
-	      nav.fadeOut('fast');
-	   }
-      else {
-         if (y < h*.20) {
-            nav.removeClass('opaque').fadeIn('fast');
-         }
-         else {
-            nav.addClass('opaque').fadeIn('fast');
-         }
-      }
-
+        if ( (y > h*.20) && (y < h) && ($(window).outerWidth() > 768 ) ) {
+            nav.fadeOut('fast');
+        } else {
+            if (y < h*.20) {
+                nav.removeClass('opaque').fadeIn('fast');
+            } else {
+                nav.addClass('opaque').fadeIn('fast');
+            }
+        }
 	});
 
-
-/*----------------------------------------------------*/
-/*	Modal Popup
-------------------------------------------------------*/
-
+    /* Modal Popup */
     $('.item-wrap a').magnificPopup({
-
        type:'inline',
        fixedContentPos: false,
        removalDelay: 200,
        showCloseBtn: false,
        mainClass: 'mfp-fade'
-
     });
 
     $(document).on('click', '.popup-modal-dismiss', function (e) {
-    		e.preventDefault();
-    		$.magnificPopup.close();
+        e.preventDefault();
+        $.magnificPopup.close();
     });
 
+    /*	Flexslider */
+    $('.flexslider').flexslider({
+        namespace: "flex-",
+        controlsContainer: ".flex-container",
+        animation: 'slide',
+        controlNav: true,
+        directionNav: false,
+        smoothHeight: true,
+        slideshowSpeed: 7000,
+        animationSpeed: 600,
+        randomize: false,
+    });
 
-/*----------------------------------------------------*/
-/*	Flexslider
-/*----------------------------------------------------*/
-   $('.flexslider').flexslider({
-      namespace: "flex-",
-      controlsContainer: ".flex-container",
-      animation: 'slide',
-      controlNav: true,
-      directionNav: false,
-      smoothHeight: true,
-      slideshowSpeed: 7000,
-      animationSpeed: 600,
-      randomize: false,
-   });
+    /*	contact form */
+    $('form#contactForm button.submit').click(function() {
+        $('#image-loader').fadeIn();
 
-/*----------------------------------------------------*/
-/*	contact form
-------------------------------------------------------*/
+        var contactName = $('#contactForm #contactName').val();
+        var contactEmail = $('#contactForm #contactEmail').val();
+        var contactSubject = $('#contactForm #contactSubject').val();
+        var contactMessage = $('#contactForm #contactMessage').val();
+        var data = 'contactName=' + contactName + '&contactEmail=' + contactEmail + '&contactSubject=' + contactSubject + '&contactMessage=' + contactMessage;
 
-   $('form#contactForm button.submit').click(function() {
-
-      $('#image-loader').fadeIn();
-
-      var contactName = $('#contactForm #contactName').val();
-      var contactEmail = $('#contactForm #contactEmail').val();
-      var contactSubject = $('#contactForm #contactSubject').val();
-      var contactMessage = $('#contactForm #contactMessage').val();
-
-      var data = 'contactName=' + contactName + '&contactEmail=' + contactEmail +
-               '&contactSubject=' + contactSubject + '&contactMessage=' + contactMessage;
-
-      $.ajax({
-
-	      type: "POST",
-	      url: "inc/sendEmail.php",
-	      data: data,
-	      success: function(msg) {
-
-            // Message was sent
-            if (msg == 'OK') {
-               $('#image-loader').fadeOut();
-               $('#message-warning').hide();
-               $('#contactForm').fadeOut();
-               $('#message-success').fadeIn();   
+        $.ajax({
+            type: "POST",
+            url: "inc/sendEmail.php",
+            data: data,
+            success: function(msg) {
+                if (msg == 'OK') {
+                    $('#image-loader').fadeOut();
+                    $('#message-warning').hide();
+                    $('#contactForm').fadeOut();
+                    $('#message-success').fadeIn();   
+                } else {
+                    $('#image-loader').fadeOut();
+                    $('#message-warning').html(msg);
+                    $('#message-warning').fadeIn();
+                }
             }
-            // There was an error
-            else {
-               $('#image-loader').fadeOut();
-               $('#message-warning').html(msg);
-	            $('#message-warning').fadeIn();
-            }
-
-	      }
-
-      });
+        });
       return false;
    });
-
-
 });
 
 
